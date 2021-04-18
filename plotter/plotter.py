3bceddd6563deb361f4a1a219b0437c37a649c0b
tilenmiklavic <tilen.miklavic@gmail.com>
2021-04-18 09:32:52 +0200

    Fixed plotter program

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import json
 import numpy as np
+import datetime
 from os import listdir
 from os.path import isfile, join 
 
@@ -21,7 +22,12 @@ def listFiles(dir):
 
 # function for plotting a graph from JSON data
 def plotGraph(file1, file2): 
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
 
+    """
+        plotting tweets data
+    """
     dates = []
     tweets = []
 
@@ -36,22 +42,37 @@ def plotGraph(file1, file2):
                 print("Not int")
 
 
+    """
+        plotting prices data
+    """
     price_dates = []
     prices = []
 
     with open(PATH_PRICE + "/" + file2) as json_file:
         data = json.load(json_file)
-        print(data)
         for p in data["prices"]:
             try:
                 a = float(p["close"])
                 prices.append(a)
-                price_dates.append(np.datetime64(p["date"]))
+                price_dates.append(np.datetime64(datetime.datetime.strptime(p["date"], '%d-%m-%Y')))
             except ValueError:
                 print("Erorr with prices")
 
 
-    plt.plot(dates, tweets)
+    print("dates: {0} tweets: {1}".format(len(dates), len(tweets)))
+    print("dates: {0} prices: {1}".format(len(price_dates), len(prices)))
+
+
+    ax1.plot(dates, tweets, label="Tweets over time", color='green')
+    ax1.tick_params(axis='y', labelcolor='green')
+    ax2.plot(price_dates, prices, label="Price over time", color='blue')
+    ax2.tick_params(axis='y', labelcolor='blue')
+
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Tweets', color='green')
+    ax2.set_ylabel('Price', color='blue')
+
+    fig.tight_layout()
     plt.show()
     print("Plotting complete")
 
