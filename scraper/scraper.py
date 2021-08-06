79e46f8d762f1656b4239124519b4d7eabbed2a3
tilenmiklavic <tm7321@student.uni-lj.si>
2021-08-06 18:38:25 +0200

    Scraper finished

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,12 +1,61 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from os import listdir
+from os.path import isfile, join
+import json
 
-url = 'https://bitinfocharts.com/dogecoin/'
-browser = webdriver.Chrome()
-browser.get(url)
+PATH_COINS = 'coins.json'
+OUTPUT_PATH = 'results.json'
 
-element = browser.find_element_by_xpath('//tr[td[a[contains(text(), "Tweets per day")]]]/td[2]/a')
 
-print(element.text)
+# function for returning a list of all coins
+def listFiles(file1):
+    coins = []
 
-browser.close()
+    # open json file containing list of all crypto coins
+    with open(file1) as json_file:
+        data = json.load(json_file)
+        for p in data["coins"]:
+            try:
+                coins.append(p)
+            except ValueError:
+                print("Error", p["name"])
+
+    return coins
+
+
+def writeResults(data, file1):
+    with open(OUTPUT_PATH + file1, 'w') as outfile:
+        json.dump(data, outfile)
+
+
+# function for web scraping
+def scrape(coins):
+    tweets_current = { 'tweets': []}
+    results = []
+    browser = webdriver.Chrome()
+
+    for coin in coins:
+        name = coin['name']
+        symbol = coin['symbol']
+
+        url = 'https://bitinfocharts.com/' + name + '/'
+        browser.get(url)
+
+        element = browser.find_element_by_xpath('//tr[td[a[contains(text(), "Tweets per day")]]]/td[2]/a')
+        tweets = element.text
+        print(tweets)
+
+        results.append({ "name": name, "tweet": tweets })
+
+    browser.close()
+    tweets_current['tweets'] = results
+    return tweets_current
+
+
+# main function
+if __name__ == '__main__':
+
+    coins = listFiles(PATH_COINS)
+    tweets = scrape(coins)
+    writeResults(tweets, "")
