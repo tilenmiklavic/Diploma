bd08d30c7c1c57c44dea69ae8ed05e1838a6b6cc
tilenmiklavic <tm7321@student.uni-lj.si>
2021-08-06 23:39:55 +0200

    Work on website

--- website/server.js
+++ website/server.js
@@ -3,7 +3,7 @@ const { readFileSync, writeFileSync } = require('fs');
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/homepage.html'))
