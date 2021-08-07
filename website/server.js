cb7c8918f9bd49d47d4b1f8863c8ac815a5bca87
tilenmiklavic <tm7321@student.uni-lj.si>
2021-08-07 22:17:34 +0200

    Website work

--- website/server.js
+++ website/server.js
@@ -1,12 +1,43 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync, writeFileSync, fstat } = require('fs');
 
 const express = require('express');
 const path = require('path')
+const url = require('url')
+const fs = require('fs')
 const app = express();
+const handlebars = require('express-handlebars')
 const port = process.env.PORT || 4000
 
+app.engine('hbs', handlebars({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/views'))
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/homepage.html'))
+    fs.readFile('coin_data.json', function(err, data) {
+        fs.readFile('results.json', function(err, results) {
+            let coin_data = JSON.parse(data)
+            let results_data = JSON.parse(results)
+
+            results_data.tweets.forEach(function(el1, ind1) {
+                coin_data.coins.forEach(function(el2, ind2) {
+                    str1 = el1.name.toLowerCase().replace(/\s/g, '')
+                    str2 = el2.coin.toLowerCase().replace(/\s/g, '')
+
+                    if (str1 == str2) {
+                        coin_data.coins[ind2].today = el1.tweet
+                    }
+                })
+            })
+            console.log(coin_data)
+            res.render('home', {
+                title: "Diploma",
+                coins: coin_data.coins
+            });
+        })
+    })
 });
 
 app.listen(port);
