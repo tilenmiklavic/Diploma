da9554d2fecc5fa82d50295958b0283a16b7cd46
tilenmiklavic <tm7321@student.uni-lj.si>
2021-08-12 23:04:26 +0200

    Website angular add

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { OgrodjeComponent } from './components/ogrodje/ogrodje.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    OgrodjeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
